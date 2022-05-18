252a9b2c1ddbc93fef9fd90c9adce7094c488c07
David Saiz <David.saiz@infomatik.hs-fulda.de>
2022-05-18 14:01:29 +0200

    AnmkerkungsFenster hinzugeFügt

--- Übung_01/scribt.js
+++ Übung_01/scribt.js
@@ -1,1 +1,8 @@
+// document.write(window.location.hostname );
+// document.write(window.location.pathname);
+// document.write(window.location.port);
+// document.write(window.location.protocol);
+
+
+
 
