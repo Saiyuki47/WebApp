248547ff3ba8ca54680b1e2ba8db73f283cce788
David Saiz <David.saiz@infomatik.hs-fulda.de>
2022-05-25 09:30:38 +0200

    JS BMI Rechner

--- Übung_01/scribt.js
+++ Übung_01/scribt.js
@@ -2,7 +2,20 @@
 // document.write(window.location.pathname);
 // document.write(window.location.port);
 // document.write(window.location.protocol);
-
+// document.write(document.getElementById("groesseID").value + document.getElementById("gewichtID").value)
+function calcBMI()
+{
+    //document.write(document.getElementById("groesseID").value+"         " + document.getElementById("gewichtID").value)
+    let height = document.getElementById("groesseID").value;
+    let fett = document.getElementById("gewichtID").value;
+    if(height>3)
+    {
+        height=height/100;
+    }
+    
+    let bmi = fett / (height*height);
+    document.getElementById("bmiValue").innerText =(fett / (height*height)).toFixed(2);
+}
 
 
 
