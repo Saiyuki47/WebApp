af712785b8d66b6b72bec0c91b77f859555343f7
David Saiz <David.saiz@infomatik.hs-fulda.de>
2022-06-29 09:30:00 +0200

    RandoRechteck(nicht fertig)

---
+++ Random Rechteck/script.js
@@ -0,0 +1,38 @@
+"use strict";
+class Rechteck
+{
+    constructor(top, left, width, height, color)
+    {
+        this._top    = top;
+        this._left   = left;
+        this._width  = width;
+        this._height = height;
+        this._color  = color;
+    }
+    draw(context)
+    {
+
+        context.fillStyle = this._color;
+        context.fillRect(this._left,
+                        this._top,
+                        this._width,
+                        this._height);
+    }
+}
+let canvas = document.getElementById("theCanvas");
+let context = canvas.getContext("2d");
+
+document.getElementById("theCanvas").addEventListener("click",randomEck(),true);
+
+function randomEck()
+{
+    var neuEck = new Rechteck(Math.floor(Math.random*500),
+                            Math.floor(Math.random*500),
+                            Math.floor(Math.random*200),
+                            Math.floor(Math.random*200),
+                            
+                            String(rgb(Math.floor(Math.random*256),
+                            Math.floor(Math.random*256),
+                            Math.floor(Math.random*256))));
+    neuEck.draw(context);
+}
